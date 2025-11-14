798dbf25b7af5acaed8682a9745c38609fbc8c4d
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-11-14 22:24:07 +0800

    feat: make telescope enable search hidden files with exceptions

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -65,6 +65,20 @@ return {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
+          fyler = {},
+        },
+
+        pickers = {
+          live_grep = {
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+            additional_args = function(_)
+              return { '--hidden' }
+            end,
+          },
+          find_files = {
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+            hidden = true,
+          },
         },
       }
 

f34089f7adc723eccd5a35679b2aae25d37b2be6
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-11-17 16:24:57 +0800

    feat: bring back oil again

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -52,6 +52,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Toggle oil
--- vim.keymap.set('n', '-', '<Cmd>Oil<CR>', { desc = 'Toggle oil' })
+vim.keymap.set('n', '-', '<Cmd>Oil<CR>', { desc = 'Toggle oil' })
 
 -- vim: ts=2 sts=2 sw=2 et

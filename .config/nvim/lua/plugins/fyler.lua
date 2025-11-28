56c344b92c0b3a0c629729ceb83f8a6760b5eb4d
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-11-28 14:49:16 +0800

    feat: change fyler mappings

--- .config/nvim/lua/plugins/fyler.lua
+++ .config/nvim/lua/plugins/fyler.lua
@@ -6,6 +6,18 @@ return {
   opts = {
     views = {
       finder = {
+        mappings = {
+          ['q'] = 'CloseView',
+          ['<CR>'] = 'Select',
+          ['<C-t>'] = 'SelectTab',
+          ['<C-v>'] = 'SelectVSplit',
+          ['<C-x>'] = 'SelectSplit',
+          ['^'] = 'GotoParent',
+          ['='] = 'GotoCwd',
+          ['.'] = 'GotoNode',
+          ['#'] = 'CollapseAll',
+          ['<BS>'] = 'CollapseNode',
+        },
         close_on_select = false,
         default_explorer = true,
         watcher = {
